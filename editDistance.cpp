a6fa2de1dc0ba59daa229b56467d2afe38c4028b
nxnxnx <internisti.jr@gmail.com>
2015-11-12 17:37:25 +0700

    no more upper/lower case problem

--- editDistance.cpp
+++ editDistance.cpp
@@ -1,5 +1,5 @@
 //editDistance.cpp
-
+#include <algorithm>
 #include <stdio.h>
 #include <string>
 #include <map>
@@ -7,6 +7,9 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+#include <utility>
+#include <string>
+#include <vector>
 
 #define READ(f) freopen(f,"r",stdin)
 #define WRITE(f) freopen(f,"w",stdin)
@@ -14,17 +17,23 @@
 using namespace std;
 
 map <string,int> sentence;
+vector<pair <int, string> > words;
+typedef vector<pair <int, string> > VECTOR;
 
 
 void entrySentence(string inp){
-  if (sentence[inp] ==NULL) sentence[inp] = 1;
-  else sentence[inp]++;
-
+  transform(inp.begin(),inp.end(),inp.begin(), ::tolower);
+  if (sentence[inp] ==NULL){
+    sentence[inp] = 1;
+  }
+  else {
+    sentence[inp]++;
+  }
 }
 
 int main(){
   READ("hound.txt");
-  
+  //WRITE("outHound.txt");
   while(!cin.eof()){
 
 		  string input;
@@ -49,10 +58,20 @@ int main(){
 		    //entrySentence(input);
 		  }//end while loop
   }
-	  
+
+
   for (map<string,int>::iterator a=sentence.begin();a!=sentence.end();++a){ //test outuput map
-    cout<< a->first<<" "<<a->second<<endl;
+    words.push_back(make_pair(a->second,a->first));
+    cout<< a->first<<"  "<<a->second<<endl;
   }
 
+  sort(words.begin(),words.end());
+  /*
+  for(const auto&p : words){
+    cout<<p.first <<" "<<p.second<<endl;
+  }*/
 
+  for (VECTOR::const_iterator pos=words.begin();pos != words.end(); pos++){
+    cout<< pos->first<<" "<<pos->second<<endl;
+  }
 }//end
